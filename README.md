e5f4189143f557f9a38cd4d6e742256797c3ca8e
Abgr Xenda <github@xendagroup.com>
2023-01-18 20:13:11 +0300

    changes in 2023
    Note 4: As of 2023, this works on Edge
    
    Note 5: example `svgSparkle($('img.logo),5,1000)` which is the jQuery for image with class logo, showing 5 sparkles for 1 second

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ Note 2: Performance isn't great
 
 Note 3: It wont work with any version of IE due to lack of SVG animation support
 
+Note 4: As of 2023, this works on Edge
+
+Note 5: example `svgSparkle($('img.logo),5,1000)` which is the jQuery for image with class logo, showing 5 sparkles for 1 second
+
 ##Demo
 
 [projects.lukehaas.me/svg-sparkle](http://projects.lukehaas.me/svg-sparkle)
--- sparkle.js
+++ sparkle.js
@@ -1,67 +1,55 @@
-var svgSparkle = function(elem) {
+var svgSparkle = function (elem, vcount, vinterval) {
+	// the vcount for the number of sparkles at a time
+	// the vinterval for how long the sparkle would appear in ms 1000 = 1second
+  var i = 0,
+    x1 = elem.offset().left - 17, //17 is roughly half the size of the svg
+    x2 = x1 + elem.width(),
+    y1 = elem.offset().top - 17,
+    y2 = y1 + elem.height(),
+    img = [],
+    decider;
 
-	var i = 0,
-		x1 = elem.offset().left-17,//17 is roughly half the size of the svg
-		x2 = x1+elem.width(),
-		y1 = elem.offset().top-17,
-		y2 = y1+elem.height(),
-		img = [],
-		decider;
+  (function addSparkle(undefined) {
+    if (img[i] === undefined) {
+      //img[i] = document.createElement("IMG");
+      img[i] = new Image();
 
+      img[i].width = 35;
+      img[i].height = 35;
 
-	(function addSparkle(undefined) {
-		
+      img[i].id = "sparkle" + i;
+      img[i].src = "/sparkle.svg?t=" + new Date().getTime(); //make sure to use the full path to the svg starting from the root
 
-		if(img[i]===undefined) {
+      img[i].onload = function () {
+        document.body.appendChild(this);
+      };
+    } else {
+      img[i].style.display = "none";
+      img[i].src = "/sparkle.svg?t=" + new Date().getTime(); // if in root/assets/images then use "/assets/images/sparkle.svg?t="
+      img[i].onload = function () {
+        this.style.display = "block";
+      };
+    }
 
-			//img[i] = document.createElement("IMG");
-			img[i] = new Image();
-			
-			img[i].width = 35;
-			img[i].height = 35;
+    img[i].style.left = Math.random() * (x2 - x1) + x1 + "px";
+    img[i].style.top = Math.random() * (y2 - y1) + y1 + "px";
 
-			img[i].id = "sparkle"+i;
-			img[i].src = "sparkle.svg?t=" + new Date().getTime();
+    decider = parseInt(Math.random() * 4 + 1);
 
-			img[i].onload = function() {
-				
-				document.body.appendChild(this);
-			};
-			
-		} else {
-			img[i].style.display = "none";
-			img[i].src = "sparkle.svg?t=" + new Date().getTime();
-			img[i].onload = function() {
-				this.style.display = "block";
-			};
-		}
+    if (decider == 2) {
+      img[i].className = "sparkle small-sparkle1";
+    } else if (decider == 3) {
+      img[i].className = "sparkle small-sparkle2";
+    } else {
+      img[i].className = "sparkle";
+    }
 
+    i += 1;
 
-		
+    if (i > vcount) {
+      i = 0;
+    }
 
-		
-		img[i].style.left = ((Math.random() * (x2-x1)) + x1) + "px";
-		img[i].style.top = ((Math.random() * (y2-y1)) + y1) + "px";
-
-		decider = parseInt((Math.random() * 4)+1);
-
-		if(decider==2) {
-			img[i].className = "sparkle small-sparkle1";
-		} else if(decider==3) {
-			img[i].className = "sparkle small-sparkle2";
-		} else {
-			img[i].className = "sparkle";
-		}
-		
-
-		i+=1;
-
-		if(i>30) {
-
-			i = 0;
-		}
-
-
-		setTimeout(addSparkle,300);
-	}());
+    setTimeout(addSparkle, vinterval);
+  })();
 };
